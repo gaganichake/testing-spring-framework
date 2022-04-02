54cf7930e3fc26bae60e84836fcf6850d95a96ab
Gagan Ichake <gaganichake@yahoo.com>
2022-04-02 17:39:41 -0500

    Integration test with Spring config

--- src/test/java/org/springframework/samples/petclinic/yannylaurel/HearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/yannylaurel/HearingInterpreterTest.java
@@ -2,17 +2,20 @@ package org.springframework.samples.petclinic.yannylaurel;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
 public class HearingInterpreterTest {
 
+    // Using Spring context to Autowire the bean
+    @Autowired
     HearingInterpreter hearingInterpreter;
-
-    @Before
-    public void setUp() throws Exception {
-        hearingInterpreter = new HearingInterpreter(new LaurelWordProducer());
-    }
 
     // A Simple JUnit 4 test
     @Test
