db44c1a3505c368b3a892bee5df4e50bf8cebe75
Gagan Ichake <gaganichake@yahoo.com>
2022-04-02 18:07:25 -0500

    Create JUnit 5 Test with Spring Context

---
+++ src/test/java/org/springframework/samples/petclinic/yannylaurel/junit5/HearingInterpreterYannyTest.java
@@ -0,0 +1,23 @@
+package org.springframework.samples.petclinic.yannylaurel.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.yannylaurel.BaseConfig;
+import org.springframework.samples.petclinic.yannylaurel.HearingInterpreter;
+import org.springframework.samples.petclinic.yannylaurel.YannyConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringJUnitConfig(classes = {BaseConfig.class, YannyConfig.class})
+class HearingInterpreterYannyTest {
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    public void whatIHeardIsYanny() {
+        String word = hearingInterpreter.whatIHeard();
+        assertEquals("Yanny", word);
+    }
+}
