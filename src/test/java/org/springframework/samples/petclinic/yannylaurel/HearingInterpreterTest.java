703547901ceff3b5b237461fe7b3cee9c85de628
Gagan Ichake <gaganichake@yahoo.com>
2022-04-02 18:08:51 -0500

    Clean up imports

--- src/test/java/org/springframework/samples/petclinic/yannylaurel/HearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/yannylaurel/HearingInterpreterTest.java
@@ -1,13 +1,12 @@
 package org.springframework.samples.petclinic.yannylaurel;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class) // This is Spring Framework runner, not Spring Boot
 @ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
