71a6b5cc59cb908facfc8476da91c6de179503f2
Gagan Ichake <gaganichake@yahoo.com>
2022-04-03 16:06:09 -0500

    Using Argument Captor and Form Parameters

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,15 +1,30 @@
 package org.springframework.samples.petclinic.web;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.reset;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ExtendWith(MockitoExtension.class)
 @SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
 
@@ -22,9 +37,17 @@ class OwnerControllerTest {
 
     MockMvc mockMvc;
 
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
+
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build(); //given
+    }
+
+    @AfterEach
+    void tearDown() {
+        reset(clinicService);
     }
 
     @Test
@@ -40,6 +63,26 @@ class OwnerControllerTest {
         mockMvc.perform(get("/owners").param("lastName", "Do Not Find Me")) // when, given
                 .andExpect(view().name("owners/findOwners")) // then
                 .andExpect(status().isOk()); // then
+    }
+
+    // Two tests: Last Name as null and returning multiple Owners
+    @Test
+    void testProcessFindFormFindByNameFoundMany() throws Exception {
+
+        Collection<Owner> ownersList = List.of(new Owner(), new Owner());
+
+        given(clinicService.findOwnerByLastName("")).willReturn(ownersList);
+
+        mockMvc.perform(get("/owners")) // when not passing Last Name then the Last Name will be null.
+                .andExpect(model().attributeExists("selections")) //then
+                .andExpect(view().name("owners/ownersList")) // then
+                .andExpect(status().isOk()); // then
+
+        // Argument Captor will capture the current value of the Last Name (which an empty String now).
+        then(clinicService).should().findOwnerByLastName(stringArgumentCaptor.capture());
+
+        // Assert that value Last Name of the Last Name is an empty String now (Using Argument Captor).
+        assertThat(stringArgumentCaptor.getValue()).isEqualTo("");
     }
 
 }
