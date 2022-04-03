b70aa336f03e5e79d07f4bce46f540147d4c560e
Gagan Ichake <gaganichake@yahoo.com>
2022-04-03 11:39:04 -0500

    Test Get Method with Spring MVC Test

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,9 +1,14 @@
 package org.springframework.samples.petclinic.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
@@ -15,8 +20,19 @@ class OwnerControllerTest {
     @Autowired
     ClinicService clinicService;
 
-    @Test
-    void testSomething() {
-        //tests
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build(); //given
     }
+
+    @Test
+    void testInitCreationForm() throws Exception {
+        mockMvc.perform(get("/owners/new")) // when
+                .andExpect(model().attributeExists("owner")) //then
+                .andExpect(view().name("owners/createOrUpdateOwnerForm")) //then
+                .andExpect(status().isOk()); //then
+    }
+
 }
